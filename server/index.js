b07de82fb7d37607c2bcdfdfe9439773e9acdb10
Patricia de Boer <pdeboer05@gmail.com>
2020-04-02 17:00:25 -0500

    Working server script and serving up bundle successfully and rendering on localhost

--- server/index.js
+++ server/index.js
@@ -42,7 +42,7 @@ app.use((err, req, res, next) => {
 
 const init = async () => {
   try {
-    await db.sync();
+    // await db.sync();
     app.listen(PORT, () =>
       console.log(`listening on port http://localhost:${PORT}`)
     );
